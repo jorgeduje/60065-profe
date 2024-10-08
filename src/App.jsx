e37a8eb784c7fb7174187949e8010848d2f57449
jorgeduje <jorgeduje4@gmail.com>
2024-10-08 19:05:13 -0300

    se limpia main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-// import { Footer } from "./components/layout/footer/Footer";
-import Counter from "./components/common/counter/Counter";
 import { Navbar } from "./components/layout/navbar/Navbar";
-import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <ItemListContainer /> */}
-      {/* <Footer /> */}
-      <Counter />
     </>
   );
 }
