bf88e309c070ebe882911e397db9622407681219
jorgeduje <jorgeduje4@gmail.com>
2024-10-17 18:57:49 -0300

    se adapta clase 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Navbar } from "./components/layout/navbar/Navbar";
-import FetchingData from "./components/pages/fetchingData/FetchingData";
 import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <ItemListContainer /> */}
-      <FetchingData />
+      <ItemListContainer />
     </>
   );
 }
