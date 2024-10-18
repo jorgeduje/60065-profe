91ac9018b0be7e73f12ba5d126621296c09638b8
jorgeduje <jorgeduje4@gmail.com>
2024-10-18 19:01:17 -0300

    se limpia la rama main

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { Comments } from "./Comments";
 import { Navbar } from "./components/layout/navbar/Navbar";
-import ItemDetailContainer from "./components/pages/itemDetail/ItemDetailContainer";
 import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
-import Posts from "./Posts";
-import Users from "./Users";
-
 function App() {
   return (
     <>
       <Navbar />
-      {/* <ItemListContainer /> */}
-      {/* <ItemDetailContainer /> */}
-      {/* <Users /> */}
-      <Posts />
-      {/* <Comments /> */}
+      <ItemListContainer />
     </>
   );
 }
